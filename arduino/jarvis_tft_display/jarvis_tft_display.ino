8b5545ee0a200ccf075c3d1331f0743bf0f07975
shabesa <k.a.shabesa@gmail.com>
2020-12-02 17:54:31 +0530

    tft settings update

--- arduino/jarvis_tft_display/jarvis_tft_display.ino
+++ arduino/jarvis_tft_display/jarvis_tft_display.ino
@@ -20,7 +20,19 @@ TSPoint tp;
 
 int16_t BOXSIZE;
 int16_t PENRADIUS = 3;
-uint16_t ID, oldcolor, currentcolor;
+uint16_t ID;
+
+// Assign human-readable names to some common 16-bit color values:
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+#define CGREEN  0x4F12
+#define PURPLE  0xA25C
 
 void setup() {
   // put your setup code here, to run once:
