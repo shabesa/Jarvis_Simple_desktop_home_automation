59c9968fa80007a993e66d614d305a5ec5789f3d
shabesa <k.a.shabesa@gmail.com>
2020-12-02 17:52:17 +0530

    tft settings

--- arduino/jarvis_tft_display/jarvis_tft_display.ino
+++ arduino/jarvis_tft_display/jarvis_tft_display.ino
@@ -1,3 +1,27 @@
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;       // hard-wired for UNO shields anyway.
+#include <TouchScreen.h>
+
+const int YM=6,YP=A2,XM=A1,XP=7;
+const int TS_LEFT=907,TS_RT=136,TS_TOP=942,TS_BOT=139;
+
+char data;
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+TSPoint tp;
+
+#define MINPRESSURE 200
+#define MAXPRESSURE 1000
+
+#define TS_MINX 940
+#define TS_MINY 160
+#define TS_MAXX 160
+#define TS_MAXY 970
+
+int16_t BOXSIZE;
+int16_t PENRADIUS = 3;
+uint16_t ID, oldcolor, currentcolor;
+
 void setup() {
   // put your setup code here, to run once:
 
